aec14c50a23dfe5fc63e4a56d031fbe5937248a5
Daniel Monroy <daniel.monroy90@gmail.com>
2025-08-02 18:06:57 -0500

    last commit for testing

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,18 +1,16 @@
 import { gql } from '@apollo/client';
 
 export const GET_CHARACTERS = gql`
-  query GetCharacters($name: String, $status: String, $species: String, $gender: String) {
-  characters(filter: { name: $name, status: $status, species: $species, gender: $gender }) {
-    results {
-      id
-      name
-      image
-      species
-      status
-      gender
+  query {
+    characters(page: 1) {
+      results {
+        id
+        name
+        species
+        image
+      }
     }
   }
-}
 `;
 
 export const GET_CHARACTER = gql`
